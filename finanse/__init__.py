ac74754b4bde0955e76b5f73530adf7184bde3f4
Piotr Wilczyński <bevesce@gmail.com>
2016-08-01 22:54:14 +0200

    fix embarassing typo

--- finanse/__init__.py
+++ finanse/__init__.py
@@ -6,7 +6,7 @@ from .money import Money
 from .money import GroupedMoney
 from .money import currency
 from .query import query
-from . import raport
+from . import report
 from .exceptions import ParseError
 from .exceptions import MoneyParseError
 from .exceptions import TransactionParseError
